bfd9eb7524ad25335af23e72c4f9aa952352b5cd
Mostafa-04 <mostafaalmannani@gmail.com>
2026-08-12 17:00:10 +0100

    ajouter deeder setting

--- database/seeders/SettingSeeder.php
+++ database/seeders/SettingSeeder.php
@@ -2,16 +2,36 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Setting;
 use Illuminate\Database\Seeder;
 
 class SettingSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
+        Setting::create([
+            'email' => 'contact@example.com',
+            'phone' => '+212 5 22 00 00 00',
+            'address' => 'Casablanca, Morocco',
+
+            'facebook' => null,
+            'linkedin' => null,
+            'twitter' => null,
+            'youtube' => null,
+
+            /*
+            |--------------------------------------------------------------------------
+            | SMTP Configuration
+            |--------------------------------------------------------------------------
+            */
+
+            'mail_host' => 'smtp.gmail.com',
+            'mail_port' => 587,
+            'mail_username' => 'mostafaalmannani@gmail.com',
+            'mail_password' => 'lukudfdyziqwawaf',
+
+            'mail_from_address' => 'mostafaalmannani@gmail.com',
+            'mail_from_name' => 'The Social Observatory',
+        ]);
     }
 }
