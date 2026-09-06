1e6da0142aa7ce41a6265277b85a1ab64534f291
Mostafa-04 <mostafaalmannani@gmail.com>
2026-09-06 17:11:23 +0100

    Force HTTPS scheme in production to fix mixed content

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,24 +6,23 @@ use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
+
         RateLimiter::for('email-campaign', function () {
-            return Limit::perMinute(60); 
+            return Limit::perMinute(60);
         });
     }
 }
